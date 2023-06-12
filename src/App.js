0ce74f17fa62f4666344dd6444747a9d4123d575
zahrabola <95353397+zahrabola@users.noreply.github.com>
2023-06-12 19:32:23 +0100

    debugging challenge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
+import React, { useState, useCallback } from "react";
 import './App.css';
+const functionsCounter = new Set();
 
-function App() {
+const howManyFunctionCalled = (increment, decrement, incrementOtherCounter) => {
+  functionsCounter.add(increment);
+  functionsCounter.add(decrement);
+  functionsCounter.add(incrementOtherCounter);
+  console.log(functionsCounter.size);
+};
+
+export function App() {
+  const [counter, setCounter] = useState(0);
+  const [otherCounter, setOtherCounter] = useState(0);
+
+  const increment = useCallback(() => {
+    setCounter(counter + 1);
+    howManyFunctionCalled(increment, decrement, incrementOtherCounter);
+    console.log("Inc");
+  }, [counter]);
+
+  const decrement = useCallback(() => {
+    setCounter(counter - 1);
+    howManyFunctionCalled(increment, decrement, incrementOtherCounter);
+    console.log("Inc");
+  }, [counter]);
+
+  const incrementOtherCounter = useCallback(() => {
+    setOtherCounter(otherCounter - 1);
+    howManyFunctionCalled(increment, decrement, incrementOtherCounter);
+    console.log("Inc");
+  }, [otherCounter]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        <code>useCallback()</code>
+      </h1>
+      <h3>Counter Value:{counter}</h3>
+      <h3>Other CounterValue:{otherCounter}</h3>
+      <h3>{`No of function calls: ${functionsCounter.size}`}</h3>
+      <button onClick={decrement}>Decrement -</button>
+      <button onClick={increment}>Increment +</button>
+      <button onClick={incrementOtherCounter}>IncrementOtherCounter +</button>
     </div>
   );
 }
 
 export default App;
+/*
+import React, { useState, useCallback } from "react";
+import "./styles.css";
+
+//// Instructions 
+//////Currently when a user clicks on inc and dec the unnecessary functions are called there by performance is getting effected. See if you can fix the uncessary function calls 
+
+const functionsCounter = new Set();
+
+const howManyFunctionCalled = (increment, decrement, incrementOtherCounter) => {
+  functionsCounter.add(increment);
+  functionsCounter.add(decrement);
+  functionsCounter.add(incrementOtherCounter);
+  console.log(functionsCounter.size);
+};
+
+export default function App() {
+  const [counter, setCounter] = useState(0);
+  const [otherCounter, setOtherCounter] = useState(0);
+
+  const increment = useCallback(() => {
+    setCounter(counter + 1);
+    howManyFunctionCalled(increment, decrement, incrementOtherCounter);
+    console.log("Inc");
+  }, [counter]);
+
+  const decrement = useCallback(() => {
+    setCounter(counter - 1);
+    howManyFunctionCalled(increment, decrement, incrementOtherCounter);
+    console.log("Inc");
+  }, [counter]);
+
+  const incrementOtherCounter = useCallback(() => {
+    setOtherCounter(otherCounter - 1);
+    howManyFunctionCalled(increment, decrement, incrementOtherCounter);
+    console.log("Inc");
+  }, [otherCounter]);
+
+  return (
+    <div className="App">
+      <h1>
+        <code>useCallback()</code>
+      </h1>
+      <h3>Counter Value:{counter}</h3>
+      <h3>Other CounterValue:{otherCounter}</h3>
+      <h3>{`No of function calls: ${functionsCounter.size}`}</h3>
+      <button onClick={decrement}>Decrement -</button>
+      <button onClick={increment}>Increment +</button>
+      <button onClick={incrementOtherCounter}>IncrementOtherCounter +</button>
+    </div>
+  );
+}
+*/
